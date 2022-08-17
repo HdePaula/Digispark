a10bdc1c83451b9ae740edf80b88ceb58271b1c6
Henrique de Paula <henriquedepaula1@hotmail.com>
2022-08-17 20:04:41 -0300

    loop de repeticao

--- fecha_tudo_e_desliga/fecha_tudo_e_desliga.ino
+++ fecha_tudo_e_desliga/fecha_tudo_e_desliga.ino
@@ -1,55 +1,26 @@
-// A simple script to have the DigiSpark constantly spam ALT+F4 and CTRL+W key combos forcing all active windows and browsers to close
+/*Usei a forma de abrir o cmd que mais achei eficiente,
+ *evitando alguns erros quando o foco de digitacao nao estava livre.
+ *
+ *Altere o valor de "n" para definir o maximo de abas a serem fechadas.
+ */
 
 #include "DigiKeyboard.h"
-void setup() {
+void setup() { 
 }
 
 void loop() {
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(20);
-  DigiKeyboard.sendKeyStroke(KEY_W, MOD_CONTROL_LEFT);
-  DigiKeyboard.delay(20);
-  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(20);
-  DigiKeyboard.sendKeyStroke(KEY_W, MOD_CONTROL_LEFT);
-  DigiKeyboard.delay(20);
-  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(20);
-  DigiKeyboard.sendKeyStroke(KEY_W, MOD_CONTROL_LEFT);
-  DigiKeyboard.delay(20);
-  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(20);
-  DigiKeyboard.sendKeyStroke(KEY_W, MOD_CONTROL_LEFT);
-  DigiKeyboard.delay(20);
-  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);  
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(20);
-  DigiKeyboard.sendKeyStroke(KEY_W, MOD_CONTROL_LEFT);
-  DigiKeyboard.delay(20);
-  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
-  DigiKeyboard.delay(20);
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(20);
-  DigiKeyboard.sendKeyStroke(KEY_W, MOD_CONTROL_LEFT);
-  DigiKeyboard.delay(20);
-  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(20);
-  DigiKeyboard.sendKeyStroke(KEY_W, MOD_CONTROL_LEFT);
-  DigiKeyboard.delay(20);
-  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(20);
-  DigiKeyboard.sendKeyStroke(KEY_W, MOD_CONTROL_LEFT);
-  DigiKeyboard.delay(20);
-  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(20);
+  int n = 6;
+  
+  for (int i = 0; i < n; i++) {
+    DigiKeyboard.sendKeyStroke(0);
+    DigiKeyboard.delay(20);
+    DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
+  }
+
   DigiKeyboard.sendKeyStroke(KEY_W, MOD_CONTROL_LEFT);
   DigiKeyboard.delay(20);
   DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  
+  for(;;){ /*empty*/ }
 }
